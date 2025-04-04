93261d96836082efd27576ae519d32c18fd8c20f
Yaroslav <strokov.media@gmail.com>
2025-04-04 16:31:13 +0300

    - add second view

---
+++ moneyCounterV2/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  moneyCounterV2
+//
+//  Created by Ярослав Строков on 04.04.2025.
+//
+
+import SwiftUI
+
+struct SecondViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondViewController()
+}
